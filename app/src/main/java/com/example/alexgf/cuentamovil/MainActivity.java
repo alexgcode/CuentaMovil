07e8dd12e0936a2ad8fd26441696e530d317162a
ALEXGF <alexgf2703@gmail.com>
2019-01-12 20:16:21 -0500

    Versión 0.1 Se creo la plantilla basica de la activity 1. Se creo la funcionalidad de mostrar la cantidad ingresada en "monto", al boton "Añadir"

--- app/src/main/java/com/example/alexgf/cuentamovil/MainActivity.java
+++ app/src/main/java/com/example/alexgf/cuentamovil/MainActivity.java
@@ -2,12 +2,42 @@ package com.example.alexgf.cuentamovil;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText et1, et2;
+    private TextView tv1;
+    private Button btn1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        et1 = (EditText)findViewById(R.id.txt_description);
+        et2 = (EditText)findViewById(R.id.txt_amount);
+        tv1 = (TextView)findViewById(R.id.txt_total);
+        btn1 = (Button)findViewById(R.id.btn_add);
+    }
+
+    public void addAmount(View view){
+        String stringDesc = et1.getText().toString();
+        String stringAmount = et2.getText().toString();
+
+        float amount;
+
+        if(stringAmount != null){
+            amount = Float.parseFloat(stringAmount);
+        }else {
+            amount = 0;
+        }
+
+        String result = String.valueOf(amount);
+        tv1.setText(result);
     }
 }
